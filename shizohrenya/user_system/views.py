234fe2eb43c1796e0d89d835d2860ef20e888325
TimAnys <109108903+TimAnys@users.noreply.github.com>
2022-11-20 01:26:21 +0300

    add profile system add rating of num completed tasks

--- shizohrenya/user_system/views.py
+++ shizohrenya/user_system/views.py
@@ -1,12 +1,16 @@
+import random
+import time
+
 from django.contrib.auth import authenticate, login
+from django.core.exceptions import ObjectDoesNotExist
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
-
 
 # Create your views here.
 from django.views import View
 
 from .forms import UserRegistrationForm
+from .models import CustomUser
 
 
 class Registration(View):
@@ -14,6 +18,7 @@ class Registration(View):
 
     template_name = 'registration/registration.html'
     success_url = reverse_lazy('login')
+
     def get(self, request):
         """
         Func which answers the GET request
@@ -44,3 +49,49 @@ class Registration(View):
                 'form': form,
                 'errors': form.errors
             })
+
+
+class ProfileView(View):
+    profile_template = 'user/profile.html'
+    my_profile_template = 'user/my_profile.html'
+    template_404 = 'user/404_error.html'
+
+    def get(self, request, pk):
+        try:
+            user = CustomUser.objects.get(username=pk)
+        except ObjectDoesNotExist:
+            return render(request, self.template_404)
+
+        context = {
+            'user': user,
+            'rating': self.rating()
+        }
+
+        if user == request.user and request.user.is_authenticated:
+            return render(request, self.my_profile_template, context)
+        else:
+            return render(request, self.profile_template, context)
+
+    def rating(self) -> list:
+        """Get all states of users
+        :return sorted states of users"""
+        all_users = CustomUser.objects.all()
+        state = []
+        for user in all_users:
+            state.append([user.username, user.completed_tasks.count()])
+        state.sort(key=lambda x: x[1],reverse=True)
+        return state
+
+        # def sort(self, list: list) -> list:
+        """Fast sorting of list
+        :return sorted list
+        """
+        # if len(list) <= 1:
+        # return list
+        # else:
+        # q = random.choice(list)
+        # l_list = [n for n in list if n < q]
+
+        # e_list = [q] * list.count(q)
+        # b_list = [n for n in list if n > q]
+        # return self.sort(l_list) + e_list + self.sort(b_list)
